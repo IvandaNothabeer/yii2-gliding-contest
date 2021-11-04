e276b8b731c5a324256c3e8e2d3ebe69f73e633c
Ivanda Nothabeer <kossaw.eve@gmail.com>
2021-11-04 21:54:04 +1300

    Tidy Contest Setup

--- config/web.php
+++ config/web.php
@@ -31,7 +31,15 @@ $config = [
 			// send all mails to a file by default. You have to set
 			// 'useFileTransport' to false and configure a transport
 			// for the mailer to send real emails.
-			'useFileTransport' => true,
+            'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'encryption' => 'tls',
+                'host' => 'localhost',
+                'port' => '465',
+                'username' => 'your_username',
+                'password' => 'your_password',
+            ],
 		],
 		'log' => [
 			'traceLevel' => YII_DEBUG ? 3 : 0,
